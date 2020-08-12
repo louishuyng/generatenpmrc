18b9389260b0f827b789d3d192fd6d63500d7bf0
Louis <huynguyennbk@gmail.com>
2020-08-12 19:04:09 +0700

    Test Flow

--- index.js
+++ index.js
@@ -3,27 +3,26 @@ const artifact = require("@actions/artifact");
 
 const generate = require("./.scripts/generate-npmrc");
 
-try {
-  const token = core.getInput("token");
-  const nameRepo = core.getInput("name-repo");
+async function main() {
+  try {
+    const token = core.getInput("token");
+    const nameRepo = core.getInput("name-repo");
 
-  generate(token, nameRepo);
+    await generate(token, nameRepo);
 
-  const artifactClient = artifact.create();
-  const artifactName = "louis-artifact";
-  const files = [".npmrc"];
+    const artifactClient = artifact.create();
+    const artifactName = "louis-artifact";
+    const files = [".npmrc"];
 
-  const rootDirectory = "."; // Also possible to use __dirname
-  const options = {
-    continueOnError: false,
-  };
+    const rootDirectory = "."; // Also possible to use __dirname
+    const options = {
+      continueOnError: false,
+    };
 
-  artifactClient.uploadArtifact(
-    artifactName,
-    files,
-    rootDirectory,
-    options
-  );
-} catch (error) {
-  core.setFailed(error.message);
+    await artifactClient.uploadArtifact(artifactName, files, rootDirectory, options);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+main();
